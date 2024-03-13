9e222f82b763eaada26d5b054d0da84e3e2607da
Efhowell <52275540+Efhowell@users.noreply.github.com>
2024-03-12 21:16:54 -0500

    Add parsing logic for subroutine calls in SubCall.java

--- SubCall.java
+++ SubCall.java
@@ -4,6 +4,27 @@ import java.util.List;
 public class SubCall implements Statement {
     private String name;
     private List<Expression> arguments;
+public SubCall(TokenStream tokens) throws Exception {
+    this.name = tokens.next().toString();
+
+    if (!tokens.next().toString().equals("(")) {
+        throw new Exception("SYNTAX ERROR: Expected '(' after subroutine name in call");
+    }
+
+    this.arguments = new ArrayList<>();
+    Token nextToken = tokens.lookAhead();
+    while (!nextToken.toString().equals(")")) {
+        this.arguments.add(new Expression(tokens));
+        nextToken = tokens.lookAhead();
+        if (nextToken.toString().equals(",")) {
+            tokens.next(); // Consume the comma
+            nextToken = tokens.lookAhead();
+        } else if (!nextToken.toString().equals(")")) {
+            throw new Exception("SYNTAX ERROR: Expected ',' or ')' in subroutine call arguments list");
+        }
+    }
+    tokens.next(); // Consume the closing ')'
+}
 
     public SubCall(String name, List<Expression> args) {
         this.name = name;
