e3120ea6d1916bda2fb3566a90cecc2061b932d8
Efhowell <52275540+Efhowell@users.noreply.github.com>
2024-03-12 21:24:45 -0500

    Convert Statement from abstract class to interface

--- Statement.java
+++ Statement.java
@@ -1,19 +1,19 @@
 /**
- * Abstract class for representing a statement in the SILLY language.
+ * Interface for representing a statement in the SILLY language.
  *   @author Dave Reed
  *   @version 12/22/23
  */
-public abstract class Statement {
-    public abstract void execute() throws Exception;
-    public abstract String toString();
-    
+public interface Statement {
+    void execute() throws Exception;
+    String toString();
+
     /**
      * Static method that reads in an arbitrary Statement.
      *   @param input the TokenStream from which the program is read
      *   @return the next Statement in the program
      */
-    public static Statement getStatement(TokenStream input) throws Exception {
-        Token first = input.lookAhead(); 
+    static Statement getStatement(TokenStream input) throws Exception {
+        Token first = input.lookAhead();
 
         if (first.toString().equals("print")) {
             return new Print(input);
@@ -32,6 +32,10 @@ public abstract class Statement {
         }
         else {
             throw new Exception("SYNTAX ERROR: Unknown statement type (" + first + ")");
+        }
+    }
+}
+
         }
     }
 }
